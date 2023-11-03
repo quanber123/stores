d205a95cd33404d9aa489a45ddd03945193a7a1b
quanber123 <quanber123@icloud.com>
2023-11-03 16:17:02 +0700

    test

--- src/views/BlogDetailsViews.tsx
+++ src/views/BlogDetailsViews.tsx
@@ -2,7 +2,7 @@ import { useParams } from 'react-router-dom';
 
 function BlogDetailsViews() {
   const { id } = useParams();
-
+  console.log(id);
   return <main className='pt-[120px]'></main>;
 }
 
