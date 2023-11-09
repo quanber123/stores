b8faa8e52893720943a51b4651203dd43b63d1bf
quanber123 <quanber123@icloud.com>
2023-11-09 18:26:52 +0700

    test

--- src/App.tsx
+++ src/App.tsx
@@ -3,7 +3,6 @@ import { Suspense, lazy, useEffect } from 'react';
 import { useDispatch } from 'react-redux';
 import Loading from './components/common/Loading';
 import { blogs, categories } from './fake-data/data';
-import { products } from './fake-data/data';
 import { setAllBlogs } from './store/slice/blogSlice';
 import { setAllProducts } from './store/slice/productSlice';
 import { setAllCategories } from './store/slice/categorySlice';
