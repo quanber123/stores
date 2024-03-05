e02f03b11f0c1097e59fd4f575ffe7d5dd99f946
quanber123 <quantm103@gmail.com>
2024-03-06 05:42:38 +0700

    [fix] service workers

--- src/main.tsx
+++ src/main.tsx
@@ -6,6 +6,7 @@ import { Provider } from 'react-redux';
 import { store } from './services/redux/store';
 import './index.css';
 import AuthProvider from './context/AuthProvider';
+
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
     <Provider store={store}>
@@ -15,18 +16,40 @@ ReactDOM.createRoot(document.getElementById('root')!).render(
     </Provider>
   </React.StrictMode>
 );
+
 console.log(import.meta.env.VITE_ENVIRONMENT);
 console.log(import.meta.env.VITE_BACKEND_URL);
+
 if (
   'serviceWorker' in navigator &&
   import.meta.env.VITE_ENVIRONMENT === 'production'
 ) {
   navigator.serviceWorker
-    .register('/sw.js')
-    .then((registration) => {
-      console.log('Service Worker registered with scope:', registration.scope);
+    .getRegistrations()
+    .then((registrations) => {
+      for (let registration of registrations) {
+        registration
+          .unregister()
+          .then((success) => {
+            console.log('Service Worker unregistered:', success);
+            navigator.serviceWorker
+              .register('/sw.js')
+              .then((registration) => {
+                console.log(
+                  'Service Worker registered with scope:',
+                  registration.scope
+                );
+              })
+              .catch((error) => {
+                console.error('Error registering Service Worker:', error);
+              });
+          })
+          .catch((error) => {
+            console.error('Error unregistering Service Worker:', error);
+          });
+      }
     })
     .catch((error) => {
-      console.error('Error registering Service Worker:', error);
+      console.error('Error getting Service Worker registrations:', error);
     });
 }
