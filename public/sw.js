a31717dd2ca17b95dd1b5fb4e523a1996f61f5b7
quanber123 <quantm103@gmail.com>
2024-03-06 05:20:08 +0700

    [fix] recache service workers

--- public/sw.js
+++ public/sw.js
@@ -3,7 +3,18 @@ const cacheName = 'CozaStore-cache';
 self.addEventListener('install', (event) => {
   event.waitUntil(
     caches.open(cacheName).then((cache) => {
-      return cache.addAll(['/', '/index.html', '/manifest.json']);
+      return caches
+        .keys()
+        .then((keys) => {
+          return Promise.all(
+            keys
+              .filter((key) => key !== cacheName)
+              .map((key) => caches.delete(key))
+          );
+        })
+        .then(() => {
+          return cache.addAll(['/', '/index.html', '/manifest.json']);
+        });
     })
   );
 });
