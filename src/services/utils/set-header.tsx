2c0830b9ee394074e84b28996b361a50f2ae7e0f
quanber123 <quantm103@gmail.com>
2024-03-17 04:54:35 +0700

    [fix] helmet

--- src/services/utils/set-header.tsx
+++ src/services/utils/set-header.tsx
@@ -3,11 +3,13 @@ import { capitalize } from './format';
 import { Helmet } from 'react-helmet';
 import { useSelector } from 'react-redux';
 import { getAllTags } from '../redux/slice/labelSlice';
+
 type Props = {
   title: string;
   description?: string;
   isBlockIndex: boolean;
 };
+
 const SetHeader: React.FC<Props> = ({ title, description, isBlockIndex }) => {
   const newTitle = useMemo(() => {
     return capitalize(title.split('/')[1]);
@@ -17,38 +19,24 @@ const SetHeader: React.FC<Props> = ({ title, description, isBlockIndex }) => {
   const setHeaderKeywords = useMemo(() => {
     return tags.map((t) => t.name).join(',');
   }, [tags]);
+
   return (
-    <>
+    <Helmet>
       {title && (
-        <Helmet>
-          <title>{newTitle ? `${newTitle} | CozaStore` : 'CozaStore'}</title>
-        </Helmet>
+        <title>{newTitle ? `${newTitle} | CozaStore` : 'CozaStore'}</title>
       )}
       {description && (
-        <Helmet>
+        <>
           <meta property='og:description' content={description} />
-        </Helmet>
-      )}
-      {description && (
-        <Helmet>
           <meta name='description' content={description} />
-        </Helmet>
+        </>
       )}
-      {tags && (
-        <Helmet>
-          <meta name='keywords' content={setHeaderKeywords} />
-        </Helmet>
-      )}
-      {isBlockIndex ? (
-        <Helmet>
-          <meta name='robots' content='noindex, nofollow' />
-        </Helmet>
-      ) : (
-        <Helmet>
-          <meta name='robots' content='index, follow' />
-        </Helmet>
-      )}
-    </>
+      {tags.length > 0 && <meta name='keywords' content={setHeaderKeywords} />}
+      <meta
+        name='robots'
+        content={isBlockIndex ? 'noindex, nofollow' : 'index, follow'}
+      />
+    </Helmet>
   );
 };
 
