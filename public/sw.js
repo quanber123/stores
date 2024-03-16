d57e6519d977abc19935b922f2a75a725ec5a554
quanber123 <quantm103@gmail.com>
2024-03-17 04:09:29 +0700

    [fix] service workers

--- public/sw.js
+++ public/sw.js
@@ -2,20 +2,26 @@ const cacheName = 'CozaStore-cache';
 
 self.addEventListener('install', (event) => {
   event.waitUntil(
-    caches.open(cacheName).then(async (cache) => {
-      await cache.addAll(['/', '/index.html', '/manifest.json']);
+    caches.open(cacheName).then((cache) => {
+      return caches
+        .keys()
+        .then((keys) => {
+          return Promise.all(
+            keys
+              .filter((key) => key !== cacheName)
+              .map((key) => caches.delete(key))
+          );
+        })
+        .then(() => {
+          return cache.addAll(['/', '/index.html', '/manifest.json']);
+        });
     })
   );
 });
-
 self.addEventListener('fetch', (event) => {
   event.respondWith(
     caches.match(event.request).then((response) => {
       return response || fetch(event.request);
     })
   );
-});
-
-self.addEventListener('activate', (event) => {
-  event.waitUntil(self.clients.claim());
 });
